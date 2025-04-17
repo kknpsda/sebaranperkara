2191037b74e119039b228e9f506b11d4a10af228
kknpsda <whentyshafiz@gmail.com>
2025-04-17 09:58:47 +0700

    Add files via upload

---
+++ layers.js
@@ -0,0 +1,56 @@
+var wms_layers = [];
+
+
+        var lyr_GoogleSatelliteHybrid_0 = new ol.layer.Tile({
+            'title': 'Google Satellite Hybrid',
+            'type':'base',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+            attributions: ' ',
+                url: 'https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}'
+            })
+        });
+var format_ADMINISTRASIDESA_AR_25K_1 = new ol.format.GeoJSON();
+var features_ADMINISTRASIDESA_AR_25K_1 = format_ADMINISTRASIDESA_AR_25K_1.readFeatures(json_ADMINISTRASIDESA_AR_25K_1, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_ADMINISTRASIDESA_AR_25K_1 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_ADMINISTRASIDESA_AR_25K_1.addFeatures(features_ADMINISTRASIDESA_AR_25K_1);
+var lyr_ADMINISTRASIDESA_AR_25K_1 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_ADMINISTRASIDESA_AR_25K_1, 
+                style: style_ADMINISTRASIDESA_AR_25K_1,
+                popuplayertitle: 'ADMINISTRASIDESA_AR_25K',
+                interactive: true,
+                title: '<img src="styles/legend/ADMINISTRASIDESA_AR_25K_1.png" /> ADMINISTRASIDESA_AR_25K'
+            });
+var format_bidang_2 = new ol.format.GeoJSON();
+var features_bidang_2 = format_bidang_2.readFeatures(json_bidang_2, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_bidang_2 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_bidang_2.addFeatures(features_bidang_2);
+var lyr_bidang_2 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_bidang_2, 
+                style: style_bidang_2,
+                popuplayertitle: 'bidang',
+                interactive: true,
+                title: '<img src="styles/legend/bidang_2.png" /> bidang'
+            });
+
+lyr_GoogleSatelliteHybrid_0.setVisible(true);lyr_ADMINISTRASIDESA_AR_25K_1.setVisible(true);lyr_bidang_2.setVisible(true);
+var layersList = [lyr_GoogleSatelliteHybrid_0,lyr_ADMINISTRASIDESA_AR_25K_1,lyr_bidang_2];
+lyr_ADMINISTRASIDESA_AR_25K_1.set('fieldAliases', {'KDPPUM': 'KDPPUM', 'NAMOBJ': 'NAMOBJ', 'REMARK': 'REMARK', 'KDPBPS': 'KDPBPS', 'FCODE': 'FCODE', 'LUASWH': 'LUASWH', 'UUPP': 'UUPP', 'SRS_ID': 'SRS_ID', 'LCODE': 'LCODE', 'METADATA': 'METADATA', 'KDEBPS': 'KDEBPS', 'KDEPUM': 'KDEPUM', 'KDCBPS': 'KDCBPS', 'KDCPUM': 'KDCPUM', 'KDBBPS': 'KDBBPS', 'KDBPUM': 'KDBPUM', 'WADMKD': 'WADMKD', 'WIADKD': 'WIADKD', 'WADMKC': 'WADMKC', 'WIADKC': 'WIADKC', 'WADMKK': 'WADMKK', 'WIADKK': 'WIADKK', 'WADMPR': 'WADMPR', 'WIADPR': 'WIADPR', 'TIPADM': 'TIPADM', 'SHAPE_Leng': 'SHAPE_Leng', 'SHAPE_Area': 'SHAPE_Area', });
+lyr_bidang_2.set('fieldAliases', {'id': 'id', 'No Perkara': 'No Perkara', 'Sertipikat': 'Sertipikat', 'Status': 'Status', 'Desa': 'Desa', 'Kecamatan': 'Kecamatan', });
+lyr_ADMINISTRASIDESA_AR_25K_1.set('fieldImages', {'KDPPUM': 'TextEdit', 'NAMOBJ': 'TextEdit', 'REMARK': 'TextEdit', 'KDPBPS': 'TextEdit', 'FCODE': 'TextEdit', 'LUASWH': 'TextEdit', 'UUPP': 'TextEdit', 'SRS_ID': 'TextEdit', 'LCODE': 'TextEdit', 'METADATA': 'TextEdit', 'KDEBPS': 'TextEdit', 'KDEPUM': 'TextEdit', 'KDCBPS': 'TextEdit', 'KDCPUM': 'TextEdit', 'KDBBPS': 'TextEdit', 'KDBPUM': 'TextEdit', 'WADMKD': 'TextEdit', 'WIADKD': 'TextEdit', 'WADMKC': 'TextEdit', 'WIADKC': 'TextEdit', 'WADMKK': 'TextEdit', 'WIADKK': 'TextEdit', 'WADMPR': 'TextEdit', 'WIADPR': 'TextEdit', 'TIPADM': 'TextEdit', 'SHAPE_Leng': 'TextEdit', 'SHAPE_Area': 'TextEdit', });
+lyr_bidang_2.set('fieldImages', {'id': '', 'No Perkara': '', 'Sertipikat': '', 'Status': '', 'Desa': '', 'Kecamatan': '', });
+lyr_ADMINISTRASIDESA_AR_25K_1.set('fieldLabels', {'KDPPUM': 'no label', 'NAMOBJ': 'no label', 'REMARK': 'no label', 'KDPBPS': 'no label', 'FCODE': 'no label', 'LUASWH': 'no label', 'UUPP': 'no label', 'SRS_ID': 'no label', 'LCODE': 'no label', 'METADATA': 'no label', 'KDEBPS': 'no label', 'KDEPUM': 'no label', 'KDCBPS': 'no label', 'KDCPUM': 'no label', 'KDBBPS': 'no label', 'KDBPUM': 'no label', 'WADMKD': 'no label', 'WIADKD': 'no label', 'WADMKC': 'no label', 'WIADKC': 'no label', 'WADMKK': 'no label', 'WIADKK': 'no label', 'WADMPR': 'no label', 'WIADPR': 'no label', 'TIPADM': 'no label', 'SHAPE_Leng': 'no label', 'SHAPE_Area': 'no label', });
+lyr_bidang_2.set('fieldLabels', {'id': 'no label', 'No Perkara': 'inline label - visible with data', 'Sertipikat': 'inline label - visible with data', 'Status': 'inline label - visible with data', 'Desa': 'inline label - visible with data', 'Kecamatan': 'inline label - visible with data', });
+lyr_bidang_2.on('precompose', function(evt) {
+    evt.context.globalCompositeOperation = 'normal';
+});
